cea10c0f920526de9778395cd7e4a6ca0797facd
USURIO <USURIO@K-AYALA>
2021-08-28 21:34:51 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio/Controllers/PersonaController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicio.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        public ActionResult procesar(String usuario, String DUI)
+        {
+            if (usuario.Equals("Katerine") && DUI.Equals("12345678"))
+            ViewBag.total = usuario;
+            ViewBag.total = DUI;
+            {
+                return Redirect("/Persona/segundavista");
+            }
+            return Redirect("/Persona/Index");
+        }
+
+        public ActionResult segundavista()
+        {
+            return View();
+        }
+
+        public ActionResult MostrarDatosProcesados(String nombre, String hrstrab, String antiguedad, String valorH)
+        {   
+
+            ViewBag.nombre = nombre;
+            ViewBag.antiguedad = antiguedad;
+            ViewBag.hrstrab = hrstrab;
+            ViewBag.valorH = valorH;
+
+            float totalacobrar = (float.Parse(valorH) * float.Parse(hrstrab)) + float.Parse(antiguedad) * 30;
+            ViewBag.total = totalacobrar;
+   
+            return View();
+        }
+    }
+}
